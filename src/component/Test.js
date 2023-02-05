f62b1537a1d5763f2b2a0d4b8635bac0a378d08b
Wasay Rizwani <wasayrizwani@gmail.com>
2023-02-05 13:41:36 +0500

    Record Changes Done

--- src/component/Test.js
+++ src/component/Test.js
@@ -1,70 +1,26 @@
-
 import React from 'react';
 import {useState, useRef,useEffect} from 'react'
 import "../Stylings/taketest.css";
-// import  MediaRecorder from "./MediaRecorder"
-import { useReactMediaRecorder } from "react-media-recorder";
+// import ReactPlayer from 'react-player';
 
-import React from 'react'
-import "../Stylings/taketest.css"
 import {
-
     Link
   } from "react-router-dom";
 
 export default function Test() {
-
     const [isPlaying, setIsPlaying] = useState(false);
     const videoRef = useRef(null);
     const [Number, setNumber] = useState(0);
     const [QuestionPath, setQuestionPath] = useState(["http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4","b.mp4"]);
     const [currentVideo, setCurrentVideo] = useState(QuestionPath[Number]);
-    const { status, startRecording, stopRecording, mediaBlobUrl } =useReactMediaRecorder({ video: true });
-
-
-
 
     useEffect(() => {
         setCurrentVideo(QuestionPath[Number]);
         console.log("Number is "+Number);
         videoRef.current.load();
-        startRecording();
-        stopRecording();
       }, [Number]);
       
-
-    async function convertMediaBlobUrlToBlob(url) {// Converting the blob url to blob
-        const response = await fetch(url)
-        .then(response => response.blob())
-        .then(response => {
-            const file=new File([response], "video.webm", {type: "video/webm"});
-            console.log("File is "+file);
-            const videoElement = document.createElement('video');
-            videoElement.controls = true;
-            videoElement.src = URL.createObjectURL(file);
-            document.body.appendChild(videoElement);
-            return file;
-        })
-    }
-
-    const SendVideotoServer=(File)=>{// File is the blob file Making API call to the server
-        const form = new FormData();
-        form.append('file', File);
-        console.log("Form is "+form);
-        fetch('http://localhost:5000/upload', {
-            method: 'POST',
-            body: form
-        })
-        .then(response => response.json())
-        .then(response => {
-            console.log("Response is "+response);
-        }
-        )
-    }
-
-    const togglePlay = () => {
-        startRecording();
-
+      const togglePlay = () => {
           if (isPlaying) {
             videoRef.current.pause();
           } else {
@@ -72,21 +28,7 @@ export default function Test() {
           }
             setIsPlaying(!isPlaying);
       };
-      const ProcessRecording = () => {
-        videoRef.current.pause();
-        stopRecording();
-        console.log("Stop Recording");
-        
-        console.log("Media Blob URL is "+mediaBlobUrl);
-
-        // Way to convert the blob url to blob
-        convertMediaBlobUrlToBlob(mediaBlobUrl);
-          
-        
-    
-      };
- 
-    const changeQuestion=()=>{
+      const changeQuestion=()=>{
         if(Number===0)
         {
           setNumber(1);
@@ -97,12 +39,6 @@ export default function Test() {
         }
         console.log("Clicked the change Question ");
     }
-
-    
-   
-
-
-
 
   return (
 <div>
@@ -159,11 +95,7 @@ export default function Test() {
                         </span>Analytics</Link>
             </li>
             <li className="sidebuttonBlock">
-
                 <Link to="/recommendation">
-
-                <Link a="/recommendation">
-
                     <span className="material-symbols-outlined">
                         recommend
                         </span> Recommend Exercises
@@ -183,7 +115,6 @@ export default function Test() {
         </div>
         <div className='vidflexouter'>
             <div className='vidflex'>
-
             {/* height="90%" width="100%" */}
             {/* <video>
                 <source src={video1} type='video/mp4' />
@@ -203,14 +134,14 @@ export default function Test() {
                     <span className = 'start_text'>Start Recording</span>
                 </button>
 
-                <button type="button" onClick={ProcessRecording} className='stop'>
+                <button type="button" className='stop'>
                     <span className="material-symbols-outlined">
                         stop_circle
                     </span>
                     <span className = 'stop_text'>End Recording</span>
                 </button>
 
-                <button type="button"  className='stop'>
+                <button type="button" className='stop'>
                     <span className="material-symbols-outlined">
                         delete_forever
                     </span>
@@ -227,44 +158,9 @@ export default function Test() {
 
             </div>
         </div>
- 
-        </div>
-    </div>
-</div>
-
-
-                <video src="vid1.mp4" controls className='video1' height="90%" width="100%"></video>
-                <div className='buttons'>
-                    <button type="button" className='next'>
-            <span className = 'next_text'>Previous</span>
-            <span className="material-symbols-outlined">
-                skip_previous
-            </span>
-        </button>
-                    <button type="button" className='next'>
-            <span className="material-symbols-outlined">
-                skip_next
-            </span>
-            <span className = 'next_text'>Next</span>
-        </button>
-                    <button type="button" className='stop'>
-            <span className="material-symbols-outlined">
-                stop_circle
-            </span>
-            <span className = 'stop_text'>End Recording</span>
-        </button>
-                    <button type="button" className='stop'>
-            <span className="material-symbols-outlined">
-                delete_forever
-            </span>
-            <span className = 'stop_text'>Cancel</span>
-             </button>
-                </div>
-        </div>
 
         </div>
     </div>
 </div>
-
 </div>
 )}
